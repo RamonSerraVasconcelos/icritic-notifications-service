87959a2a323335a5ebb14c6e4e0cf86dfef93d90
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2023-08-03 20:40:44 -0300

    Adding ExternalNotificationEntityMapper test

---
+++ src/test/java/com/icritic/notifications/dataprovider/database/mapper/ExternalNotificationEntityMapperTest.java
@@ -0,0 +1,26 @@
+package com.icritic.notifications.dataprovider.database.mapper;
+
+import com.icritic.notifications.core.fixture.ExternalNotificationFixture;
+import com.icritic.notifications.core.model.ExternalNotification;
+import com.icritic.notifications.dataprovider.database.entity.ExternalNotificationEntity;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExternalNotificationEntityMapperTest {
+
+    @Test
+    void givenExternalNotification_thenReturnExternalNotificationEntity() {
+        ExternalNotification externalNotification = ExternalNotificationFixture.load();
+
+        ExternalNotificationEntityMapper mapper = ExternalNotificationEntityMapper.INSTANCE;
+
+        ExternalNotificationEntity externalNotificationEntity = mapper.externalNotificationToExternalNotificationEntity(externalNotification);
+
+        assertEquals(externalNotification.getNotificationId(), externalNotificationEntity.getNotificationId());
+        assertEquals(externalNotification.getTopic(), externalNotificationEntity.getTopic());
+        assertEquals(externalNotification.getNotifierId(), externalNotificationEntity.getNotifierId());
+        assertEquals(externalNotification.getEmail(), externalNotificationEntity.getEmail());
+        assertEquals(externalNotification.getCreatedAt(), externalNotificationEntity.getCreatedAt());
+    }
+}
