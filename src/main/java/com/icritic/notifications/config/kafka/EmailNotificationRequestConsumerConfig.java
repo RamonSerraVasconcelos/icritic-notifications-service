3c050aea2079d696a49bedd12cc72c54d5b528cc
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2024-04-21 14:35:44 -0300

    Removing obsolete classes

--- src/main/java/com/icritic/notifications/config/kafka/EmailNotificationRequestConsumerConfig.java
+++ src/main/java/com/icritic/notifications/config/kafka/EmailNotificationRequestConsumerConfig.java
@@ -1,6 +1,5 @@
 package com.icritic.notifications.config.kafka;
 
-import com.icritic.notifications.dataprovider.kafka.entity.EmailResetRequestMessage;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,26 +11,26 @@ import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
-public class EmailResetRequestConsumerConfig {
+public class EmailResetConsumerConfig {
 
     @Autowired
     private ConsumersConfig consumersConfig;
 
     @Bean
-    public ConsumerFactory<String, EmailResetRequestMessage> emailResetRequestKafkaConsumerFactory() {
+    public ConsumerFactory<String, EmailResetMessage> emailResetKafkaConsumerFactory() {
         DefaultJackson2JavaTypeMapper jsonMapper = new DefaultJackson2JavaTypeMapper();
         jsonMapper.addTrustedPackages("*");
 
-        JsonDeserializer<EmailResetRequestMessage> jsonDeserializer = new JsonDeserializer<>(EmailResetRequestMessage.class);
+        JsonDeserializer<EmailResetMessage> jsonDeserializer = new JsonDeserializer<>(EmailResetMessage.class);
         jsonDeserializer.setTypeMapper(jsonMapper);
 
-        return new DefaultKafkaConsumerFactory<>( consumersConfig.consumerConfigs(jsonDeserializer), new StringDeserializer(), jsonDeserializer);
+        return new DefaultKafkaConsumerFactory<>(consumersConfig.consumerConfigs(jsonDeserializer), new StringDeserializer(), jsonDeserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, EmailResetRequestMessage> emailResetRequestListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, EmailResetRequestMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(emailResetRequestKafkaConsumerFactory());
+    public ConcurrentKafkaListenerContainerFactory<String, EmailResetMessage> emailResetListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, EmailResetMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(emailResetKafkaConsumerFactory());
         return factory;
     }
 }
